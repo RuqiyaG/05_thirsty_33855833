ff7b0e6dda0b27b50251d49a298a3c4c0c89a921
RuqiyaG <ruqiyageelle@gmail.com>
2025-11-04 18:11:08 +0000

    added route handlers

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/",(req,res)=>{
+    res.render("index.ejs")
+});
+
+router.get("/about",(req,res)=>{
+    res.render("about.ejs")
+});
+
+router.get("/search",(req,res)=>{
+    res.render("search.ejs")
+});
+
 module.exports = router
